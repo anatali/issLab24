d6f47b25db916ec54dddf46b6481714abe532013
Antonio Natali <agoognat@gmail.com>
2024-02-19 12:39:58 +0100

    Lez1

--- servicemath24Synch/servicemathsynchbase.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% servicemathsynchbase description   
-%====================================================================================
-request( dofibo, dofibo(N) ).
-reply( fibodone, fibodone(CALLER,N,RESULT,TIME) ).  %%for dofibo
-%====================================================================================
-context(ctxservice, "localhost",  "TCP", "8710").
- qactor( servicemath, ctxservice, "Servicecodedbasic").
- static(servicemath).
